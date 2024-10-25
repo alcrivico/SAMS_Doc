56a41229d7a8508e27b31d42ee9c4264edc70068
DracoGilga <cesar96707@gmail.com>
2024-10-25 02:50:02 -0600

    add: jobs and transactional Procedure

--- Documentación/BaseDatos/ScriptTablas.sql
+++ Documentación/BaseDatos/ScriptTablas.sql
@@ -186,3 +186,35 @@ ALTER TABLE [DetalleVenta] ADD CONSTRAINT [FK_DetalleVenta] FOREIGN KEY ([idProd
 ALTER TABLE [Producto] ADD CONSTRAINT [FK_Producto] FOREIGN KEY ([idProveedor]) REFERENCES [Proveedor]([id]);
 ALTER TABLE [Producto] ADD CONSTRAINT [FK_Producto] FOREIGN KEY ([idUnidadMedida]) REFERENCES [UnidadDeMedida]([id]);
 ALTER TABLE [PromocionVigencia] ADD CONSTRAINT [FK_PromocionVigencia] FOREIGN KEY ([idPromocion]) REFERENCES [Promocion]([id]);
+
+
+-- 1. Crear el inicio de sesión para el usuario en el nivel del servidor
+USE [master];
+GO
+CREATE LOGIN [SAMS.Data.Admin] 
+WITH PASSWORD = 'nZrYF&jArpj%f6aBAtE#';
+GO
+-- 2. Crear el usuario en la base de datos SAMS.Data
+USE [SAMS.Data];
+GO
+CREATE USER [SAMS.Data.Admin] 
+FOR LOGIN [SAMS.Data.Admin];
+GO
+-- 3. Otorgar permisos de owner solo para la base de datos SAMS.Data
+ALTER ROLE db_owner ADD MEMBER [SAMS.Data.Admin];
+GO
+-- 4. Cambiar al contexto de la base de datos msdb
+USE [msdb];
+GO
+-- 5. Crear el usuario en la base de datos msdb
+CREATE USER [SAMS.Data.Admin] 
+FOR LOGIN [SAMS.Data.Admin];
+GO
+-- 6. Agregar el usuario al rol SQLAgentUserRole en msdb
+EXEC sp_addrolemember 
+    @rolename = N'SQLAgentUserRole', 
+    @membername = N'SAMS.Data.Admin';  
+GO
+
+USE [SAMS.Data];
+GO
